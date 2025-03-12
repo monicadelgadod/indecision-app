deb2c750e7d912328be6225efdafb95e7547530a
Monica Delgado <monicadelgadod11@gmail.com>
2025-03-12 17:24:44 +0100

    Api Yesno

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -2,19 +2,17 @@
 
     <div class="p-4 flex-1 overflow-y-auto">
         <div class="flex flex-col space-y-2">
-            <div class="flex justify-end">
-                <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">
-                    Hola, ¿Cómo te va el día?
-                </div>
-            </div>
 
-            <div class="flex">
-                <div class="bg-gray-300 text-black p-2 max-w-xs rounded-lg">
-                    Muy bien, ahora estoy en casa. ¿Y tú?
-                </div>
-            </div>
+            <ChatBullula :meus-est="true" nuntius="¿Salimos luego a dar una vuelta?"/>
+
         </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+import ChatBullula from './ChatBullula.vue';
+
+
+</script>
 
 
